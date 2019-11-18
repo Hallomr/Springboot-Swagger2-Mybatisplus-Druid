84454a7faef011e5bc876c0298c5c50b040691b5
Hallomr <hp6890@outlook.com>
2019-11-18 21:45:34 +0800

    commit

--- src/main/java/com/example/mybatisplus/vo/resp/UserResp.java
+++ src/main/java/com/example/mybatisplus/vo/resp/UserResp.java
@@ -1,5 +1,6 @@
 package com.example.mybatisplus.vo.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -24,8 +25,10 @@ public class UserResp {
     private Integer status;
 
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
     @ApiModelProperty(value = "更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 }
